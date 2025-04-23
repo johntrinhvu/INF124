042d4a7648c2d6b410c8d4d8c7a08f97a6d32074
johntrinhvu <johntrinhvu@yahoo.com>
2025-04-22 17:42:31 -0700

    get tailwind to work, using tailwind 3

--- learnxr/client/src/components/Header/Header.jsx
+++ learnxr/client/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="">
-      <h3>Header</h3>
+      <h3 className="text-4xl font-bold underline">Header</h3>
     </div>
   );
 };
