d580ffb008770096a37c18e624d82bab6ded090d
alllexisf <alexisflores2411@gmail.com>
2025-05-05 08:21:09 -0700

    Linked Sign Up Continue Button to Dashboard

--- learnxr/client/src/components/SignUpCard/SignUpCard.jsx
+++ learnxr/client/src/components/SignUpCard/SignUpCard.jsx
@@ -16,7 +16,9 @@ export default function SignUpCard() {
                 <input type="text" id="emailaddress" placeholder="Email address" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
                 <input type="text" id="password" placeholder="Password" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
                 <input type="text" id="confirmpassword" placeholder="Confirm password" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
-                <button type="button" className="text-sm text-white bg-blue-700 rounded-[20px] hover:bg-blue-800 focus:ring-2 focus:ring-[#9690C4] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 p-2">Continue</button>
+                <Link to="/dashboard"
+                    className="text-sm text-white bg-blue-700 rounded-[20px] hover:bg-blue-800 focus:ring-2 focus:ring-[#9690C4] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 p-2">Continue
+                </Link>
                 <p className="text-xs text-white pb-10">Already have an account? <Link to="/signin" className="text-[#FE9324]">Sign in</Link></p>
             </div>
         </div>
