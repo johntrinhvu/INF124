63daa2f5fd09e6eadeb84ad86bd7a276b5a7f745
alllexisf <alexisflores2411@gmail.com>
2025-05-05 08:50:30 -0700

    Removed Temporary Dashboard Button

--- learnxr/client/src/components/Header/Header.jsx
+++ learnxr/client/src/components/Header/Header.jsx
@@ -28,10 +28,6 @@ export default function Header() {
             <Link to="/signup" className="bg-[#ACACE5] py-1 px-3 text-sm border border-[#10103D] rounded-sm">
               Sign Up
             </Link>
-            {/* New Dashboard Button */}
-            <Link to="/dashboard" className="text-white text-sm px-3 py-1 border border-[#10103D] bg-[#6B6DB6] rounded-sm hover:bg-[#7b7dc0] transition">
-              Dashboard
-            </Link>
           </div>
         </div>
       </nav>
