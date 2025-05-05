8cf2de76bd19cc941fcf43e4f1eed74a6131adee
alllexisf <alexisflores2411@gmail.com>
2025-05-05 08:15:34 -0700

    Linked Sign In Continue Button to Dashboard

--- learnxr/client/src/components/SignInCard/SignInCard.jsx
+++ learnxr/client/src/components/SignInCard/SignInCard.jsx
@@ -14,7 +14,9 @@ export default function SignInCard() {
                 </div>
                 <input type="text" id="emailaddress" placeholder="Email address" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
                 <input type="text" id="password" placeholder="Password" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
-                <button type="button" className="text-sm text-white bg-blue-700 rounded-[20px] hover:bg-blue-800 focus:ring-2 focus:ring-[#9690C4] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 p-2">Continue</button>
+                <Link to="/dashboard"
+                    className="text-sm text-white bg-blue-700 rounded-[20px] hover:bg-blue-800 focus:ring-2 focus:ring-[#9690C4] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 p-2">Continue
+                </Link>
                 <p className="text-xs text-white pb-10">Don't have an account? <Link to="/signup" className="text-[#FE9324]">Sign up</Link></p>
             </div>
         </div>
