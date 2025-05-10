880e18f72df4560d0be767e797df83a99285d553
smjsmsam <smjsmsam@gmail.com>
2025-05-10 16:24:40 -0700

    update for readability

--- learnxr/client/src/components/SignInCard/SignInCard.jsx
+++ learnxr/client/src/components/SignInCard/SignInCard.jsx
@@ -12,12 +12,23 @@ export default function SignInCard() {
                 <div className="grid justify-center">
                     <img src={Logo} alt="Logo" className="w-40 h-40" />
                 </div>
-                <input type="text" id="emailaddress" placeholder="Email address" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
-                <input type="text" id="password" placeholder="Password" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
+                <input
+                    type="text"
+                    id="emailaddress"
+                    placeholder="Email address"
+                    className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
+                <input
+                    type="text"
+                    id="password"
+                    placeholder="Password"
+                    className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
                 <Link to="/dashboard"
-                    className="text-sm text-white bg-blue-700 rounded-[20px] hover:bg-blue-800 focus:ring-2 focus:ring-[#9690C4] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 p-2">Continue
+                    className="text-sm text-white bg-blue-700 rounded-[20px] hover:bg-blue-800 focus:ring-2 focus:ring-[#9690C4] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 p-2">
+                        Continue
                 </Link>
-                <p className="text-xs text-white pb-10">Don't have an account? <Link to="/signup" className="text-[#FE9324]">Sign up</Link></p>
+                <p className="text-xs text-white pb-10">
+                    Don't have an account? <Link to="/signup" className="text-[#FE9324]">Sign up</Link>
+                </p>
             </div>
         </div>
     )
--- learnxr/client/src/components/SignUpCard/SignUpCard.jsx
+++ learnxr/client/src/components/SignUpCard/SignUpCard.jsx
@@ -12,14 +12,33 @@ export default function SignUpCard() {
                 <div className="grid justify-center">
                     <img src={Logo} alt="Logo" className="w-40 h-40" />
                 </div>
-                <input type="text" id="username" placeholder="Username" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
-                <input type="text" id="emailaddress" placeholder="Email address" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
-                <input type="text" id="password" placeholder="Password" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
-                <input type="text" id="confirmpassword" placeholder="Confirm password" className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
+                <input
+                    type="text"
+                    id="username"
+                    placeholder="Username"
+                    className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
+                <input
+                    type="text"
+                    id="emailaddress"
+                    placeholder="Email address"
+                    className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
+                <input
+                    type="text"
+                    id="password"
+                    placeholder="Password"
+                    className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
+                <input
+                    type="text"
+                    id="confirmpassword"
+                    placeholder="Confirm password"
+                    className="text-sm text-white bg-[#242452] border-2 border-[#252592] rounded-[20px] p-2" />
                 <Link to="/dashboard"
-                    className="text-sm text-white bg-blue-700 rounded-[20px] hover:bg-blue-800 focus:ring-2 focus:ring-[#9690C4] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 p-2">Continue
+                    className="text-sm text-white bg-blue-700 rounded-[20px] hover:bg-blue-800 focus:ring-2 focus:ring-[#9690C4] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 p-2">
+                        Continue
                 </Link>
-                <p className="text-xs text-white pb-10">Already have an account? <Link to="/signin" className="text-[#FE9324]">Sign in</Link></p>
+                <p className="text-xs text-white pb-10">
+                    Already have an account? <Link to="/signin" className="text-[#FE9324]">Sign in</Link>
+                </p>
             </div>
         </div>
     )
